d023871cab1930d24ac9827a11d5c4078f544b6b
DeveloperCraig <40456881+DeveloperCraig@users.noreply.github.com>
2021-05-12 20:40:26 +0100

    Add project files.

---
+++ MovieStudioAPI/Classes/MovieQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieStudioAPI.Classes
+{
+    public class MovieQuery
+    {
+        public void AddMovie(MovieData Data, string path)
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("{0},{1},{2},{3},{4}", Data.MovieId, Data.Title, Data.Language, Data.Duration, Data.ReleaseYear);
+            }
+        }
+    }
+}
---
+++ MovieStudioAPI/Controllers/MetadataController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieStudioAPI.Classes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MovieStudioAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MetadataController : ControllerBase
+    {
+        // GET: <MetadataController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET <MetadataController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST <MetadataController>
+        [HttpPost]
+        public void Post([FromBody] Metadata value)
+        {
+            try
+            {
+                var t = new MovieQuery();
+                t.AddMovie(value, Environment.CurrentDirectory + "/Docs/Database.txt" );
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            
+
+        }
+
+        // PUT <MetadataController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE <MetadataController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
